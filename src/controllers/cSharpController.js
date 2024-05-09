53865c216b32a7b7e7c73ed6073c13b5003fd2f5
Oktalon-Szoradi <szoradi.o19@htlwienwest.at>
2024-05-09 21:46:50 +0200

    Add basic infiniloop detection, handle thrown errors, truncate response

--- src/controllers/cSharpController.js
+++ src/controllers/cSharpController.js
@@ -15,13 +15,30 @@ export const runCSharp = async (req, res) => {
     res.status(400).json({ error: 'No code provided' })
     return
   }
+
+  if (
+    code.includes('while (true)') ||
+    code.includes('while(true)') ||
+    code.includes('for (;;)') ||
+    code.includes('for(;;)')
+  ) {
+    res.status(400).json({ error: 'Infinite loop detected' })
+    return
+  }
+
   if (isNullOrWhitespace(user)) {
     console.log('No user provided: ', user)
     res.status(400).json({ error: 'No user provided' })
     return
   }
 
-  const data = await model.runCSharp(code, user)
+  let data
+  try {
+    data = await model.runCSharp(code, user)
+  } catch (err) {
+    res.status(400).json({ error: err.message })
+    return
+  }
 
   if (data.timedOut) {
     res.status(408).json(data)
@@ -31,5 +48,11 @@ export const runCSharp = async (req, res) => {
     res.status(400).json(data)
     return
   }
+
+  // Limit output to 1000 characters or 100 lines
+  if (data.output.length > 512 || data.output.split('\n').length > 100) {
+    data.output = data.output.slice(0, 512) + '...'
+  }
+
   res.json(data)
 }
